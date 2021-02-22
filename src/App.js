2338d27e83b867a48567a3ea5eff173b208dc570
Ása <asajuliaa@gmail.com>
2021-02-22 19:42:52 +0000

    app.js update render

--- src/App.js
+++ src/App.js
@@ -12,25 +12,26 @@ import Skills from './contents/Skills';
 import Contact from './contents/Contact';
 
 function App() {
-return (
-<Router>
-  <div className="App">
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/education" component={Education} />
-        <Route path="/skills" component={Skills} />
-        <Route path="/contact" component={Contact} />
+   render(){
+  return (
+  <Router>
+    <div className="App">
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/education" component={Education} />
+          <Route path="/skills" component={Skills} />
+          <Route path="/contact" component={Contact} />
 
-        <div className="nav">
-            <Link to="/" className="item">Home</Link>
-            <Link to="/about" className="item">About</Link>
-            <Link to="/education" className="item">Education</Link>
-            <Link to="/skills" className="item">Skills</Link>
-            <Link to="/contact" className="item">Contact</Link>
+          <div className="nav">
+              <Link to="/" className="item">Home</Link>
+              <Link to="/about" className="item">About</Link>
+              <Link to="/education" className="item">Education</Link>
+              <Link to="/skills" className="item">Skills</Link>
+              <Link to="/contact" className="item">Contact</Link>
 
-        </div>
-  </div>
-</Router>
-)
-}
+          </div>
+    </div>
+  </Router>
+  )
+}}
 export default App;
