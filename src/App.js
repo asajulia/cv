ac38d1653c693dfca936e57379ab79bb829e5e40
Ása <asajuliaa@gmail.com>
2021-02-22 19:46:49 +0000

    app.js update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import
 {
 BrowserRouter as Router,
 Route,
 } from "react-router-dom";
+
 import { Link } from "react-router-dom";
+
 import Home from './contents/Home';
 import About from './contents/About';
 import Education from './contents/Education';
@@ -12,26 +15,25 @@ import Skills from './contents/Skills';
 import Contact from './contents/Contact';
 
 function App() {
-   render(){
-  return (
-  <Router>
-    <div className="App">
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/education" component={Education} />
-          <Route path="/skills" component={Skills} />
-          <Route path="/contact" component={Contact} />
 
-          <div className="nav">
-              <Link to="/" className="item">Home</Link>
-              <Link to="/about" className="item">About</Link>
-              <Link to="/education" className="item">Education</Link>
-              <Link to="/skills" className="item">Skills</Link>
-              <Link to="/contact" className="item">Contact</Link>
+return (
+<Router>
+  <div className="App">
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/education" component={Education} />
+        <Route path="/skills" component={Skills} />
+        <Route path="/contact" component={Contact} />
 
-          </div>
-    </div>
-  </Router>
-  )
-}}
+        <div className="nav">
+            <Link to="/" className="item">Home</Link>
+            <Link to="/about" className="item">About</Link>
+            <Link to="/education" className="item">Education</Link>
+            <Link to="/skills" className="item">Skills</Link>
+            <Link to="/contact" className="item">Contact</Link>
+        </div>
+  </div>
+</Router>
+)
+}
 export default App;
